b57220eac130eedeb5455bc8d5c3289420400278
thiago.leta <thiago.leta@trescon.com.br>
2021-04-03 09:11:09 -0300

    projeto SbAdmin template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
+
+//importando as classes para uso da biblioteca de rotas do angular
+//estas rotas serão utilizadas para navegação da SPA - Single Page Application
+import { Routes, RouterModule } from '@angular/router';
+
+//mapear uma rota (URL) para cada componente do projeto..
+const routes : Routes = [
+  { path : 'cadastro-clientes', component : CadastroClientesComponent },
+  { path : 'consulta-clientes', component : ConsultaClientesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClientesComponent,
+    ConsultaClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) //registrando a configuração de rotas!
   ],
   providers: [],
   bootstrap: [AppComponent]
